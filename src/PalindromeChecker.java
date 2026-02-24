55f577fe8eee7a51b91c4749e2be3db75bcbfba2
siv0707 <drsivasuryameenakshisundaram@gmail.com>
2026-02-24 15:11:39 +0530

    UC2:Hardcoded palindrome

---
+++ src/PalindromeChecker.java
@@ -0,0 +1,25 @@
+/**
+ *
+ */
+public class PalindromeChecker {
+
+    public static void main(String[] args) {
+
+        // Hardcoded input string
+        String input = "madam";
+
+        boolean isPalindrome = true;
+
+        // Loop only till half of the string length
+        for (int i = 0; i < input.length() / 2; i++) {
+
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input text: " + input);
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
+    }
+}
